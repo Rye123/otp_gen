7682bfa84055ff5348894cc5e4af958a55544d02
Ryan Toh <ryedralisk@gmail.com>
2023-08-13 11:35:11 +0800

    test: shift tests out to unittest

--- otp_gen.py
+++ otp_gen.py
@@ -47,21 +47,6 @@ def truncate(value: bytes, digits: int = 6) -> int:
     return hotp
 
 
-def truncate_test():
-    test_hex_str = '1f 86 98 69 0e 02 ca 16 61 85 50 ef 7f 19 da 8e 94 5b 55 5a'
-    test_bytes = bytes.fromhex(test_hex_str)
-    otp = truncate(test_bytes)
-
-    assert otp == 872921
-
-
-def truncate_test_2():
-    test_hex_str = 'a9 4a 8f e5 cc b1 9b a6 1c 4c 08 73 d3 91 e9 87 98 2f bb d3'
-    test_bytes = bytes.fromhex(test_hex_str)
-    otp = truncate(test_bytes)
-
-    assert otp == 913627
-
 
 def hotp_generate(key: bytes, counter: bytes, digits: int = 6) -> bytes:
     """
