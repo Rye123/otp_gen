62b70979f4812744b957b82b7e4858748e7754fa
Ryan Toh <ryedralisk@gmail.com>
2023-08-13 11:53:46 +0800

    feat: Encrypted setup keys with Fernet

---
+++ add_setupkey.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+from util import create_key_file
+from base64 import b32decode
+from binascii import Error as BinAsciiError
+
+KEYDIR = Path("./keys")
+
+if __name__ == "__main__":
+    KEYDIR.mkdir(exist_ok=True)
+    if not KEYDIR.is_dir():
+        raise Exception("Ensure ./keys is a directory.")
+
+    label = input("Enter the label to be associated with the key: ")
+    setupkey = input("Enter the setup key (no spaces): ").upper()
+
+    try:
+        b32decode(setupkey)
+    except BinAsciiError:
+        raise Exception("Invalid setup key.")
+
+    print("Enter a PIN to be associated with this setup key. This CAN be unique for each setup key, but it's easier "
+          "if you keep it the same.")
+    pin = input("PIN: ")
+
+    create_key_file(pin, label, setupkey)
+
+
--- otp_gen.py
+++ otp_gen.py
@@ -2,11 +2,11 @@ from hashlib import sha1
 import hmac
 import time
 from math import floor
+from pathlib import Path
+from util import parse_key_dir
 from base64 import b32decode
 
-KEY_plaintext = 'ORSXG5DJNZTTCMRT'
-KEY = b32decode(KEY_plaintext)
-
+KEYDIR = Path("./keys")
 
 def get_current_timestep() -> int:
     """
@@ -47,15 +47,14 @@ def truncate(value: bytes, digits: int = 6) -> int:
     return hotp
 
 
-
-def hotp_generate(key: bytes, counter: bytes, digits: int = 6) -> bytes:
+def hotp_generate(setupkey: bytes, counter: bytes, digits: int = 6) -> int:
     """
-    Obtains the OTP associated with `key` and `counter`.
+    Obtains the OTP associated with `setupkey` and `counter`.
 
     Defined in RFC4226 (https://www.rfc-editor.org/rfc/rfc4226)
 
     Inputs:
-    - `key`: A bytestring of AT LEAST 128 bits.
+    - `setupkey`: A bytestring of AT LEAST 128 bits.
     - `counter`: An 8-byte counter value, synchronised \
         between the HOTP generator (client) and the HOTP \
             validator (server).
@@ -70,36 +69,43 @@ def hotp_generate(key: bytes, counter: bytes, digits: int = 6) -> bytes:
         raise ValueError("Counter expects an 8-byte value.")
 
     # Generate HMAC-SHA1 value (20 bytes)
-    hmac_obj = hmac.new(key, counter, sha1)
+    hmac_obj = hmac.new(setupkey, counter, sha1)
     hs = hmac_obj.digest()
 
     # Truncate hs (31-bit string)
     return truncate(hs, digits)
 
 
-def totp_generate(key: bytes, t: int, digits: int = 6):
+def totp_generate(setupkey: bytes, t: int, digits: int = 6):
     """
     Generates a time-based OTP based on HOTP.
 
     Defined in RFC-6238 (https://www.ietf.org/rfc/rfc6238.txt)
 
     Inputs:
-    - `key`: Unique secret for each prover.
+    - `setupkey`: Unique secret for each prover.
     - `t`: Integer representing number of time steps between initial counter T0 and current Unix Time.
     - `digits`: Number of digits in the OTP.
     """
-    return hotp_generate(key, t.to_bytes(8, 'big'), digits)
+    return hotp_generate(setupkey, t.to_bytes(8, 'big'), digits)
 
 
-def get_totp(key: bytes):
-    return totp_generate(key, get_current_timestep())
+def get_totp(setupkey: bytes):
+    return totp_generate(setupkey, get_current_timestep())
 
 
 if __name__ == "__main__":
     time_step = 30
     prev_step = 0
     epoch_time = 0
-    print(f"Using secret key: {KEY}")
+
+    # Load keys
+    pin = input("Enter PIN: ")
+    keys = parse_key_dir(pin)
+    if len(keys) == 0:
+        print("No keys detected.")
+        exit(1)
+
     try:
         while True:
             cur_step = get_current_timestep()
@@ -107,9 +113,13 @@ if __name__ == "__main__":
                 prev_step = cur_step
                 time_str = time.asctime(time.localtime(time.time()))
                 timestep = get_current_timestep()
+
                 print(f"Time: {time_str}")
-                print(f"Previous OTP: {totp_generate(KEY, timestep-1):06d}")
-                print(f"Current OTP : {totp_generate(KEY, timestep):06d}")
-                print(f"Next OTP    : {totp_generate(KEY, timestep+1):06d}")
+                for label, key in keys.items():
+                    key_b = b32decode(key)
+                    print(f"---{label}---")
+                    print(f"Previous OTP: {totp_generate(key_b, timestep-1):06d}")
+                    print(f"Current OTP : {totp_generate(key_b, timestep):06d}")
+                    print(f"Next OTP    : {totp_generate(key_b, timestep+1):06d}")
     except KeyboardInterrupt:
         print("\nProgram Ended.")
